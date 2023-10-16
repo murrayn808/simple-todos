49a70ddb4b084b6a82e27c5f9d89b407f2e7f617
Nathaniel Murray <murrayn@hawaii.edu>
2023-10-15 20:22:33 -1000

    Section 4

--- app/imports/ui/App.jsx
+++ app/imports/ui/App.jsx
@@ -4,6 +4,16 @@ import { TasksCollection } from '/imports/api/TasksCollection';
 import { Task } from './Task';
 import { TaskForm } from './TaskForm';
 
+const toggleChecked = ({ _id, isChecked }) => {
+  TasksCollection.update(_id, {
+    $set: {
+      isChecked: !isChecked
+    }
+  })
+};
+
+const deleteTask = ({ _id }) => TasksCollection.remove(_id);
+
 export const App = () => {
   const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
 
@@ -14,7 +24,12 @@ export const App = () => {
       <TaskForm/>
 
       <ul>
-        { tasks.map(task => <Task key={ task._id } task={ task }/>) }
+        { tasks.map(task => <Task
+          key={ task._id }
+          task={ task }
+          onCheckboxClick={toggleChecked}
+          onDeleteClick={deleteTask}
+        />) }
       </ul>
     </div>
   );
